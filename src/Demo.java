a258c7311407bbd5b0ae411c0b70166ce107ceda
xiaoliqing123 <52121204+xiaoliqing123@users.noreply.github.com>
2020-02-23 21:08:39 +0800

    Update Demo.java
    第四次修改

--- src/Demo.java
+++ src/Demo.java
@@ -1,8 +1,5 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("我来了");
-        System.out.println("分支一号来袭");
-        System.out.println("哈哈哈哈");
-        System.out.println("第三次修改");
+        System.out.println("不一样的故事");
     }
 }
